73e9812f98e5a574a816aceaff6dc15848c4c919
profstrategy <ojobalaro2018@gmail.com>
2025-11-11 06:57:22 -0800

    remove unneccesary layout column

--- src/components/categories-section/CategoriesSection.tsx
+++ src/components/categories-section/CategoriesSection.tsx
@@ -118,7 +118,7 @@ const CategoriesSection = () => {
 
         {/* Category Cards Container */}
         <motion.div
-          className="flex overflow-x-auto whitespace-nowrap gap-2 pb-4 md:grid md:grid-cols-4 lg:grid-cols-6 md:gap-4 no-scrollbar"
+          className="flex overflow-x-auto whitespace-nowrap gap-2 pb-4 md:grid md:grid-cols-4 md:gap-4 no-scrollbar"
           ref={scrollRef}
           onMouseEnter={() => isMobile && setIsPaused(true)}
           onMouseLeave={() => isMobile && setIsPaused(false)}
